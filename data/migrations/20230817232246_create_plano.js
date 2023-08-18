406895d0ea114e7d4185b2a49d89509cbe40a0b2
Pedro Lutz <pedrolutz@protonmail.com>
2023-08-17 21:37:52 -0300

    gerado a tabela de recursos

--- data/migrations/20230817232246_create_plano.js
+++ data/migrations/20230817232246_create_plano.js
@@ -5,11 +5,16 @@
 exports.up = function(knex) {
     return knex.schema.createTable('plano', (table)=> {
       table.increments();
-      table.char("tipo").notNullable();
-      table.string("descricao",60).notNullable();
-      table.date("data").notNullable();
-      table.decimal("preco",5.2).notNullable();
       table.string("area",60).notNullable();
+      table.string("recurso",60).notNullable();
+      table.char("tipo_a").notNullable();
+      table.decimal("valor_a",5.2).notNullable();
+      table.string("plano_a",300).notNullable();
+      table.date("data_limite").notNullable();
+      table.string("plano_b",300).notNullable();
+      table.char("tipo_b").notNullable();
+      table.decimal("valor_b",5.2).notNullable();
+      
     });
   };
   exports.down = function(knex) {
